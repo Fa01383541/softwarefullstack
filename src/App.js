c8f3b0681b75256bed750b0ce78f619c2b3af660
Facundo Marroquín <105237134+Fa01383541@users.noreply.github.com>
2023-05-29 18:51:34 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MiComponente from './MiComponente';
+
+function App() {
+  return (
+    <div className="App">
+      <MiComponente />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MiComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MiComponente() {
+  return (
+    <div>
+      <h1>Hola, soy un componente de React</h1>
+      <p>Bienvenido a mi aplicación.</p>
+    </div>
+  );
+}
+
+export default MiComponente;
